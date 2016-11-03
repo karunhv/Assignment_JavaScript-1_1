453239c7362f8e1e8171496e38d01824dddda8a6
Karunakar HV <frontendacadglid@gmail.com>
2016-11-03 19:37:45 +0530

    Java Script Data Types and Variables
    Assignment – Declare variables using JavaScript to store your name, age, dateofbirth and placeofbirth. Display the value of
    these variables in console

---
+++ app.js
@@ -0,0 +1,9 @@
+var name = "Karunakar"; //String data type
+var age = 30; //Integer Data Type
+var dateOfBirth = "26-Sep-1986";
+var placeOfBirth = "Bangalore";
+
+console.log("Name: " + name); //Printing the name in the console.
+console.log("Age: " + age);
+console.log("Date of Birth: " + dateOfBirth);
+console.log("Place of Birth: " + placeOfBirth);
